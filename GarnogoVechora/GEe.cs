a7f998a8d018e6a5cfbb1e32ce6264e3d7aa5ca4
volimax <zzdimidrolzz@gmail.com>
2020-05-07 10:56:32 +0300

    someChanges

--- GarnogoVechora/GEe.cs
+++ GarnogoVechora/GEe.cs
@@ -6,18 +6,10 @@ namespace GarnogoVechora
     {
         static void Main(string[] args)
         {
-            Magazine f = new Magazine();            
-            f.AddArticle("Point of deception", 8.5);
-            f.AddArticle("Source", 10);
-            f.AddArticles(new Article("sds", 9.2), new Article("qweqe", 10) );
-            //foreach (Article collection in f.dobirka)
-            //{
-            //    Console.Write("Book`s title: " + collection.Title + ", ");
-            //    Console.WriteLine("Book`s rate: " + collection.Rate);
-            //}
-            //Console.WriteLine(f.ToShortString());
-            //Console.WriteLine(f.ToString());
-            
+            Magazine f = new Magazine();
+            Console.WriteLine(f.ToShortString());
+            Console.WriteLine(f.ToString());            
+            f.AddArticles(new Article("Point of deception", 9.2), new Article("Source", 10) );                                    
             Console.WriteLine(f.ToString());
         }
     }
